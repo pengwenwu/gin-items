5d6db70c2a57d9a89f822adb97771a5b3f32158b
pengwenwu <pengwenwu@corp-ci.com>
2019-11-20 14:18:21 +0800

    update:解决不定字段查询问题

--- model/item.go
+++ model/item.go
@@ -18,47 +18,13 @@ type Items struct {
 	//Props Props `json:"props"`
 }
 
-func GetItemList(offset, limit int, maps interface{}) ([]map[string]interface{}, error)  {
-	//var items []*Items
-	//err := db.Table("items").Select("name").Offset(offset).Limit(limit).Find(&items).Error
-	//if err != nil {
-	//	return nil, err
-	//}
-	//return items, nil
-	rows, err := db.Table("items").Select("name,photo,detail").Offset(offset).Limit(limit).Rows()
+func GetItemList(offset, limit int, maps interface{}) ([]map[string]string, error)  {
+	rows, err := db.Table("items").Select("name,photo,detail,state").Offset(offset).Limit(limit).Rows()
 	if err != nil {
 		return nil, err
 	}
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-	length := len(columns)
-	result := make([]map[string]interface{}, 0)
-	for rows.Next() {
-		current := makeResultReceiver(length)
-		if err := rows.Scan(current...); err != nil {
-			return nil, err
-		}
-		value := make(map[string]interface{})
-		for i := 0; i < length; i++ {
-			k := columns[i]
-			v := current[i]
-			value[k] = v
-		}
-		result = append(result, value)
-	}
+	result := Rows2SliceMap(rows)
 	return result, nil
-}
-
-func makeResultReceiver(length int) []interface{}  {
-	result := make([]interface{}, 0, length)
-	for i := 0; i < length; i++ {
-		var current interface{}
-		current = struct{}{}
-		result = append(result, &current)
-	}
-	return result
 }
 
 func GetItemTotal(maps interface{}) (int, error) {
--- model/model.go
+++ model/model.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
@@ -50,4 +51,33 @@ func init() {
 
 func CloseDB() {
 	defer db.Close()
+}
+
+// 结果集转切片
+func Rows2SliceMap(rows *sql.Rows) (list []map[string]string) {
+	//字段名称
+	columns, _ := rows.Columns()
+	//多少个字段
+	length := len(columns)
+	//每一行字段的值
+	values := make([]sql.RawBytes, length)
+	//保存的是values的内存地址
+	pointer := make([]interface{}, length)
+	//
+	for i := 0; i < length; i++ {
+		pointer[i] = &values[i]
+	}
+	//
+	for rows.Next() {
+		//把参数展开，把每一行的值存到指定的内存地址去，循环覆盖，values也就跟着被赋值了
+		rows.Scan(pointer...)
+		//每一行
+		row := make(map[string]string)
+		for i := 0; i < length; i++ {
+			row[columns[i]] = string(values[i])
+		}
+		list = append(list, row)
+	}
+	//
+	return
 }
