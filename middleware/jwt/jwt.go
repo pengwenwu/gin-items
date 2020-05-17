03bbbaf2729eba10cffa73ac1f70a964b0e68326
pengwenwu <pww932589183@163.com>
2020-05-17 23:39:35 +0800

    add:jwt中间件校验

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -1,48 +1,41 @@
 package jwt
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 
 	"gin-items/library/ecode"
+	"gin-items/library/token"
 )
-
-type result struct {
-	State ecode.Code `json:"state"`
-	Msg string `json:"msg"`
-}
 
 func Jwt() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		result := checkToken(context)
-		if result.State != ecode.OK {
+		if result.State != ecode.OK.Code() {
 			context.Abort()
 			context.JSON(http.StatusUnauthorized, result)
 		}
+		context.Set("token_data", result.Data)
 	}
 }
 
-func checkToken(context *gin.Context) (res result) {
+func checkToken(context *gin.Context) (result token.DecodeResult) {
 	authorization := context.Request.Header.Get("authorization")
-	res = result{
-		State: ecode.OK,
-		Msg: ecode.OK.Message(),
-	}
 	if len(authorization) == 0 {
-		res.State = ecode.NoAuthorization
-		res.Msg = ecode.NoAuthorization.Message()
+		result.State = ecode.NoAuthorization.Code()
+		result.Msg = ecode.NoAuthorization.Message()
 		return
 	}
 	if strings.Contains(authorization, "Bearer") == false {
-		res.State = ecode.AuthorizationErr
-		res.Msg = ecode.AuthorizationErr.Message()
+		result.State = ecode.AuthorizationErr.Code()
+		result.Msg = ecode.AuthorizationErr.Message()
 		return
 	}
 
-	token := strings.Replace(authorization, "Bearer ", "", -1)
-	fmt.Println(token)
+	tokenStr := strings.Replace(authorization, "Bearer ", "", -1)
+	tokenDecodeRes := token.UnSafeDecode(tokenStr)
+	result = tokenDecodeRes
 	return
 }
